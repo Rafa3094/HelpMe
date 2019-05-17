918068bc9114a6adb5f3d62276e7d2ecb4e6ca32
S34n4e <sean.alarcon@ucrso.info>
2019-05-17 12:41:18 -0600

    modified:   .idea/misc.xml 	modified:   .idea/vcs.xml 	modified:   app/src/main/java/com/helpme/MainActivity.java

--- app/src/main/java/com/helpme/MainActivity.java
+++ app/src/main/java/com/helpme/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity
         loadFragment(new HomeFragment());
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
