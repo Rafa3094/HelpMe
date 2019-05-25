e35ac14bf6e9c7b18b10423e4bab14b9134a0770
S34n4e <sean.alarcon@ucrso.info>
2019-05-25 11:10:59 -0600

    Layout num.emergencia y funcionalidad

--- app/src/main/java/com/helpme/Entities/EmergencyNumber.java
+++ app/src/main/java/com/helpme/Entities/EmergencyNumber.java
@@ -1,4 +1,38 @@
 package com.helpme.Entities;
 
 public class EmergencyNumber {
+
+    private String name;
+    private int phone;
+
+    public EmergencyNumber() {
+    }
+
+    public EmergencyNumber(String name, int phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+
+        return name + " \t\t " + phone;
+
+    }
 }
--- app/src/main/java/com/helpme/Fragments/InstitutionsFragment.java
+++ app/src/main/java/com/helpme/Fragments/InstitutionsFragment.java
@@ -1,20 +1,37 @@
 package com.helpme.Fragments;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.storage.StorageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.helpme.Entities.EmergencyNumber;
+import com.helpme.MainActivity;
 import com.helpme.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InstitutionsFragment extends Fragment {
 
+    View view;
+    InstitutionsFragment fragment = this;
 
     public InstitutionsFragment() {
         // Required empty public constructor
@@ -25,7 +42,39 @@ public class InstitutionsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_institutions, container, false);
+        super.onCreate(savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_institutions, container, false);
+
+        ListView lv = (ListView) view.findViewById(R.id.emergency_list);
+
+        final ArrayAdapter adapter = new ArrayAdapter<EmergencyNumber>(getActivity(), android.R.layout.simple_list_item_1 , getArrayOfArrayList());
+
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CALL_PHONE}, 1);
+                }
+                if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    String phoneNumber = "" + ((EmergencyNumber) parent.getAdapter().getItem(position)).getPhone();
+                    Intent intent = new Intent(getActivity(), InstitutionsFragment.class);
+                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber)));
+                }
+            }
+        });
+        return view;
+    }
+
+    private EmergencyNumber[] getArrayOfArrayList(){
+        EmergencyNumber[] stringArray = {
+                new EmergencyNumber("Emergency to Allan",888763595),
+                new EmergencyNumber("Emergency to Estiven",89693541),
+                new EmergencyNumber("Emergency to Kevin", 61516547),
+                new EmergencyNumber("Emergency to Rafa", 89040990),
+                new EmergencyNumber("Emergency to Sean", 87755608)
+        };
+        return stringArray;
     }
 
 }
