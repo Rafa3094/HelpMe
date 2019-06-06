2a7c8f8a20db796597c2a8a5ec79fdf2d41f327d
S34n4e <sean.alarcon@ucrso.info>
2019-06-05 21:29:03 -0600

    User guide agregado en el btn settings que ahora se llama User guide

--- app/src/main/java/com/helpme/MainActivity.java
+++ app/src/main/java/com/helpme/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(MainActivity.this, TutoActivity.class));
             return true;
         }
 
