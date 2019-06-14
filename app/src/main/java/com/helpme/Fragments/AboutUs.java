24ef5ef594ebfe9eae8c7c37e9e25172031d1b9f
luis-estiven <luis-estiven@hotmail.com>
2019-06-14 17:21:42 -0600

    color de botones

---
+++ app/src/main/java/com/helpme/Fragments/AboutUs.java
@@ -0,0 +1,30 @@
+package com.helpme.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.helpme.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutUs extends Fragment {
+
+    View view;
+    public AboutUs() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_about_us, container, false);;
+        return view;
+    }
+
+}
--- app/src/main/java/com/helpme/MainActivity.java
+++ app/src/main/java/com/helpme/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.helpme.Fragments.AboutUs;
 import com.helpme.Fragments.HomeFragment;
 import com.helpme.Fragments.InstitutionsFragment;
 import com.helpme.Fragments.UserInfoFragment;
@@ -99,6 +100,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_user) {
             loadFragment(new UserInfoFragment());
+
+        }  else if (id == R.id.nav_about_us) {
+            loadFragment(new AboutUs());
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
